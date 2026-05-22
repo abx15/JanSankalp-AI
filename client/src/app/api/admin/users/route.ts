a40661c85706a9ffccad32dd16af2e02c857311c
Arun Kumar Bind <arunkumarbind150720@gmail.com>
2026-05-22 11:26:50 +0530

    refactor: delete client/src/app/api/admin/users/route.ts

--- client/src/app/api/admin/users/route.ts
+++
@@ -1,9 +0,0 @@
-import { userController } from "@/modules/user/user.controller";
-
-export async function GET(req: Request) {
-  return userController.getAllUsers(req);
-}
-
-export async function POST(req: Request) {
-  return userController.handleAdminUserActionFromBody(req);
-}
