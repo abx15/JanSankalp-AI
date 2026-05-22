29b59333199df1bb83b6d00a77b532bba327115e
Arun Kumar Bind <arunkumarbind150720@gmail.com>
2026-05-22 11:26:48 +0530

    refactor: delete client/src/api/notifications/route.ts

--- client/src/api/notifications/route.ts
+++
@@ -1,9 +0,0 @@
-import { notificationController } from "@/modules/notifications/notification.controller";
-
-export async function GET(req: Request) {
-    return notificationController.getAll(req);
-}
-
-export async function PATCH(req: Request) {
-    return notificationController.markRead(req);
-}
