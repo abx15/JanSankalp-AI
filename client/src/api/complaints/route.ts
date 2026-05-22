a3602fa81bebe368afce6047178f86bc7434fcaa
Arun Kumar Bind <arunkumarbind150720@gmail.com>
2026-05-22 11:26:46 +0530

    refactor: delete client/src/api/complaints/route.ts

--- client/src/api/complaints/route.ts
+++
@@ -1,9 +0,0 @@
-import { complaintController } from "@/modules/complaints/complaint.controller";
-
-export async function GET(req: Request) {
-    return complaintController.getComplaints(req);
-}
-
-export async function POST(req: Request) {
-    return complaintController.createComplaint(req);
-}
